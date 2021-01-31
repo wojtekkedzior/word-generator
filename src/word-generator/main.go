a89871524868f3fdcfeea9fa61938e79ecbc17c3
Wojtek <wowaked@gmail.com>
2021-01-31 19:28:26 +0100

    asda

--- src/word-generator/main.go
+++ src/word-generator/main.go
@@ -57,7 +57,7 @@ func getNumberOfPermutations(length int) int {
 func getPermutations(permCount int, str []byte) [][]int {
 	rand.Seed(time.Now().UnixNano())
 	strOri := str
-	pos := make([][]int, 0) // this wil cause the array to be coppied immedtialy. perhaps we should use size / something?        
+	pos := make([][]int, 0) // this wil cause the array to be coppied immedtialy. perhaps we should use size / something?         
 	posWord := make(map[string]int)
 	var count = 0
 
