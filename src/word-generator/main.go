7a81a3ac87122969194f32895cbb362b73d0faaa
Wojtek <wowaked@gmail.com>
2021-04-22 13:05:28 +0200

    add a recursive approach
    Signed-off-by: Wojtek <wowaked@gmail.com>

--- src/word-generator/main.go
+++ src/word-generator/main.go
@@ -49,12 +49,12 @@ func getNumberOfPermutations(length int) int {
 		size = size + p
 	}
 
-	fmt.Printf("Number of possibilities: %d \n", size)
+	fmt.Printf("Number of possibilities for length of %d is %d \n", length, size)
 	return size
 }
 
-func writePossibleWord(pos [][]int, indicies ...int) [][]int {
-	res := make([]int, 0, 10) //set capacity to max word size
+func writePossibleWordArray(length int, indicies []int) {
+	res := make([]int, 0, length) //set capacity to max word size
 
 	for _, v := range indicies {
 		if v != 0 {
@@ -62,101 +62,67 @@ func writePossibleWord(pos [][]int, indicies ...int) [][]int {
 		}
 	}
 
-	return append(pos, res)
+	pos = append(pos, res)
 }
 
-func getPermutations(permCount int, str []byte) [][]int {
-	pos := make([][]int, 0, 9864100) // this will cause the array to be copied immedtialy. perhaps we should use size / something?
-	var count = 0
+func run(length int, index int) {
+	if index == length {
+		return
+	}
 
-	start := time.Now()
-
-	count, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10 := 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-
-	for i := 0; i < 10; i++ {
-		c1 = i
-		count++
-		pos = writePossibleWord(pos, c1)
-		for j := 0; j < 10; j++ {
-			if j == i {
-				continue
-			}
-			c2 = j
-			count++
-			pos = writePossibleWord(pos, c1, c2)
-			for k := 0; k < 10; k++ {
-				if k == j || k == i {
-					continue
-				}
-				c3 = k
-				count++
-				pos = writePossibleWord(pos, c1, c2, c3)
-				for l := 0; l < 10; l++ {
-					if l == k || l == j || l == i {
-						continue
-					}
-					c4 = l
-					count++
-					pos = writePossibleWord(pos, c1, c2, c3, c4)
-					for m := 0; m < 10; m++ {
-						if m == l || m == k || m == j || m == i {
-							continue
-						}
-						c5 = m
-						count++
-						pos = writePossibleWord(pos, c1, c2, c3, c4, c5)
-						for n := 0; n < 10; n++ {
-							if n == m || n == l || n == k || n == j || n == i {
-								continue
-							}
-							c6 = n
-							count++
-							pos = writePossibleWord(pos, c1, c2, c3, c4, c5, c6)
-							for o := 0; o < 10; o++ {
-								if o == n || o == m || o == l || o == k || o == j || o == i {
-									continue
-								}
-								c7 = o
-								count++
-								pos = writePossibleWord(pos, c1, c2, c3, c4, c5, c6, c7)
-
-								for p := 0; p < 10; p++ {
-									if p == o || p == n || p == m || p == l || p == k || p == j || p == i {
-										continue
-									}
-									c8 = p
-									count++
-									pos = writePossibleWord(pos, c1, c2, c3, c4, c5, c6, c7, c8)
-
-									for r := 0; r < 10; r++ {
-										if r == p || r == o || r == n || r == m || r == l || r == k || r == j || r == i {
-											continue
-										}
-										c9 = r
-										count++
-										pos = writePossibleWord(pos, c1, c2, c3, c4, c5, c6, c7, c8, c9)
-
-										for s := 0; s < 10; s++ {
-											if s == r || s == p || s == o || s == n || s == m || s == l || s == k || s == j || s == i {
-												continue
-											}
-											c10 = s
-											count++
-											pos = writePossibleWord(pos, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10)
-										}
-									}
-								}
-							}
-						}
-					}
-				}
+	for k := 0; k < length; k++ {
+		shouldContinue := true
+		for j := 0; j < index; j++ {
+			if k == counters[j] {
+				shouldContinue = false
+				break
 			}
 		}
+
+		if shouldContinue {
+			counters[index] = k
+			writePossibleWordArray(length, counters[0:index+1])
+			count++
+			run(length, index+1)
+		} else {
+			continue
+		}
 	}
+}
+
+var pos [][]int
+var counters []int
+var count int
+
+func getPermutations(permCount int, str []byte) [][]int {
+	realPermCount := getNumberOfPermutations(len(str) + 1)
+	pos = make([][]int, 0, realPermCount)
+	start := time.Now()
+	length := len(str) + 1
+	counters = make([]int, length)
+
+	for i := 0; i < len(counters); i++ {
+		counters[i] = 0
+	}
+
+	for i := 0; i < length; i++ {
+		run(length, i)
+	}
+
+	fmt.Printf("Number of possibilites generated: %d \n", len(pos)) //this should match realPermCount.  The recursive call generetes extra, which is a buug
+	fmt.Printf("Number of iterations to generete all permutations: %d \n", count)
+	fmt.Printf("Time to generete all permutations %s \n", time.Since(start))
+
+	return pos
+}
+
+func bruteforce(str []byte) [][]int {
 
 	// rand.Seed(time.Now().UnixNano())
 	// strOri := str
-	// pos := make([][]int, 0) // this will cause the array to be coppied immedtialy. perhaps we should use size / something?
+	realPermCount := getNumberOfPermutations(len(str) + 1)
+	pos = make([][]int, 0, realPermCount)
+
 	// posWord := make(map[string]int)
 	// var count = 0
 
@@ -202,9 +168,9 @@ func getPermutations(permCount int, str []byte) [][]int {
 	// 	count++
 	// }
 
-	fmt.Printf("Number of possibilites generated: %d \n", len(pos))
-	fmt.Printf("Number of Random iterations: %d \n", count)
-	fmt.Printf("Figuring out all the permutations took %s \n", time.Since(start))
+	// fmt.Printf("Number of possibilites generated: %d \n", len(pos))
+	// fmt.Printf("Number of Random iterations: %d \n", count)
+	// fmt.Printf("Figuring out all the permutations took %s \n", time.Since(start))
 
 	return pos
 }
@@ -352,12 +318,3 @@ func main() {
 
 	topParent.lookup([]byte(inputWord))
 }
-
-// for 9 letters
-// wojtek@wojtek-pc:~/git/word-generator/src/word-generator$ go run main.go
-// Skipped because of length: 33146, Skipped because chars don't exist in provided word: 68028.  Total skipped: 101174
-// Number of possibilities: 986409
-// Number of Random iterations: 59485463
-// Figuring out all the permutations took 23.754992341s
-// Traversing tree took 74.642873ms
-// Found a total of 264 words.
