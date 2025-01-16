d213cd1c1be6c5eb17de24ce3c46d1949a8c9fc4
Wojtek <wowaked@gmail.com>
2025-01-16 20:02:11 +0100

    a bunch of changes to optimise performance
    Signed-off-by: Wojtek <wowaked@gmail.com>

--- src/word-generator/main.go
+++ src/word-generator/main.go
@@ -3,25 +3,57 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"sync"
 	"time"
-	"unicode/utf8"
+
+	"github.com/apache/pulsar-client-go/pulsar"
 )
 
-// var segmentSize = 10000
 var lengthPlusSpace, iterations, words = 0, 0, 0
 
 var foundWords sync.Map
 var wg sync.WaitGroup
-var results = make(chan []int, 10000)
+var bufferSize = 200
+
+var numOfResults = 10000
+
+var client pulsar.Client
+var err error
 
 // Represents a char.  A link of Nodes can result in a word.
 type Node struct {
 	IsWord   bool
 	Value    rune
 	Childern map[rune]*Node
+}
+
+type Permutations struct {
+	Permutations []perm `json:"permutations,omitempty"`
+}
+
+type perm struct {
+	Permutation string `json:"permutation,omitempty"`
+}
+
+func init() {
+	client, err = pulsar.NewClient(pulsar.ClientOptions{
+		URL:                     "pulsar://192.168.122.10:4002",
+		OperationTimeout:        30 * time.Second,
+		ConnectionTimeout:       30 * time.Second,
+		MaxConnectionsPerBroker: 10,
+	})
+
+	if err != nil {
+		fmt.Println("Failed to create client", err)
+	}
+
+	defer client.Close()
 }
 
 func getNumberOfPermutations(length int) {
@@ -37,7 +69,13 @@ func getNumberOfPermutations(length int) {
 	fmt.Printf("Number of possibilities for length of %d is %d \n", length, size)
 }
 
-func run(index int, counters []int, c chan []int) {
+// index   -
+// counter -
+// results - a channel where a message container multiple permutations will be written to.
+// buffer  - contains each permutation as a comma delimited string of integers
+// permutations - holds a list of Permutations. The array is marshaled into JSON when the array size exceeds the "bufferSize"
+// permutation  - an array of indexes representing a single permutation. This array is cleared after the generated permutation is passed on.
+func run(index int, counters []int, results chan string, buffer *bytes.Buffer, permutations *Permutations, permutation []int) {
 	if index == lengthPlusSpace {
 		return
 	}
@@ -53,17 +91,71 @@ func run(index int, counters []int, c chan []int) {
 		if shouldContinue {
 			counters[index] = j
 
-			permutation := make([]int, 0, lengthPlusSpace)
 			for _, v := range counters[0 : index+1] {
 				if v != 0 { //effectively trimming
 					permutation = append(permutation, (v - 1))
 				}
 			}
 
-			c <- permutation
-			run(index+1, counters, c)
+			for _, i := range permutation {
+				buffer.WriteString(strconv.Itoa(i))
+				buffer.WriteString(",")
+			}
+
+			permutations.Permutations = append(permutations.Permutations, perm{Permutation: buffer.String()})
+			buffer.Reset()
+			permutation = permutation[:0]
+
+			if len(permutations.Permutations) > bufferSize {
+				jsonData, err := json.Marshal(permutations)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				results <- string(jsonData)
+
+				permutations.Permutations = permutations.Permutations[:0]
+			}
+
+			run(index+1, counters, results, buffer, permutations, permutation)
 		}
 	}
+}
+
+func createChannelReader(id int) chan string {
+	var results = make(chan string, numOfResults)
+	var producer pulsar.Producer
+
+	go func(id int) {
+		producer, err = client.CreateProducer(pulsar.ProducerOptions{
+			Topic:                   "t/ns/mercury",
+			BatchingMaxPublishDelay: 10 * time.Millisecond, // Maximum time to wait for batching
+			BatchingMaxMessages:     uint(500),             // Maximum number of messages in a batch
+			BatchingMaxSize:         1024 * 1024 * 10,      // Maximum size of batch (10MB)
+			Name:                    fmt.Sprintf("producer-for-channel-%v", +id),
+		})
+
+		if err != nil {
+			fmt.Println("Failed to create producer: ", err)
+		}
+
+		defer producer.Close()
+
+		for msg := range results {
+			producer.SendAsync(context.Background(), &pulsar.ProducerMessage{
+				Payload: []byte(msg),
+			}, func(msgID pulsar.MessageID, msg *pulsar.ProducerMessage, err error) {
+				if err != nil {
+					fmt.Printf("Failed to send message: %v", err)
+					return
+				}
+			})
+		}
+
+		close(results)
+	}(id)
+
+	return results
 }
 
 func (topParent Node) lookup(str []byte) {
@@ -80,47 +172,53 @@ func (topParent Node) lookup(str []byte) {
 
 	for i := 0; i < lengthPlusSpace; i++ {
 		wg.Add(1)
+
 		go func(index int) {
 			defer wg.Done()
 			counters := make([]int, lengthPlusSpace)
-			run(index, counters, results)
+			results := createChannelReader(index)
+
+			// var permutations Permutations
+			permutations := &Permutations{}
+
+			permutation := make([]int, 0, lengthPlusSpace)
+
+			buffer := bytes.NewBuffer(make([]byte, 0, bufferSize))
+			run(index, counters, results, buffer, permutations, permutation)
 		}(i)
 	}
 
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
+	wg.Wait()
 
-	for permutation := range results {
-		iterations++
+	// for permutation := range results {
+	// 	iterations++
 
-		top := &topParent
+	// 	top := &topParent
 
-		for i, vr := range permutation {
-			r, _ := utf8.DecodeRune([]byte{str[vr]})
-			n := top.Childern[r]
+	// 	for i, vr := range permutation {
+	// 		r, _ := utf8.DecodeRune([]byte{str[vr]})
+	// 		n := top.Childern[r]
 
-			if n == nil {
-				break
-			} else if i == len(permutation)-1 && n.IsWord { // is this a short word?
-				var b bytes.Buffer
+	// 		if n == nil {
+	// 			break
+	// 		} else if i == len(permutation)-1 && n.IsWord { // is this a short word?
+	// 			var b bytes.Buffer
 
-				for _, va := range permutation {
-					r, _ := utf8.DecodeRune([]byte{str[va]})
-					b.WriteRune(r)
-				}
+	// 			for _, va := range permutation {
+	// 				r, _ := utf8.DecodeRune([]byte{str[va]})
+	// 				b.WriteRune(r)
+	// 			}
 
-				foundWords.Store(b.String(), 1)
-				words++
-				break
-			} else {
-				top = top.Childern[r]
-			}
-		}
+	// 			foundWords.Store(b.String(), 1)
+	// 			words++
+	// 			break
+	// 		} else {
+	// 			top = top.Childern[r]
+	// 		}
+	// 	}
 
-		top = &topParent
-	}
+	// 	top = &topParent
+	// }
 
 	var finalResult []string
 
@@ -137,14 +235,15 @@ func (topParent Node) lookup(str []byte) {
 }
 
 func main() {
-	// inputWord := "proselytize" //11
+	inputWord := "proselytize" //11
 	// inputWord := "abandonwares" //12
 	// inputWord := "ventriloquizes" //14
 	// inputWord := "kaiserdoms" //10
 	// inputWord := "Counterrevolutionary" //20 - 6613313319248080000 possibilites :D
 	// inputWord := "planets" //7
-	inputWord := "youngster" //9
+	// inputWord := "youngster" //9
 	// inputWord := "or" //9
+	// inputWord := "helper"
 
 	var skippedDueToLength, skippedDueToChar = 0, 0
 
@@ -215,20 +314,3 @@ func main() {
   The permutations are actually indexes of the array that holds the word.
   This way we can convert each permutation into char quickly.
 */
-
-// func test(c chan int) int {
-// 	mycount := 0
-// 	for i := 0; i < 1000000; i++ {
-// 		// count++
-// 		c <- i
-// 		mycount++
-// 	}
-// 	return mycount
-// }
-
-// for complete != 8 {
-// 	select {
-// 	case <-c:
-// 		// fmt.Println(complete)
-// 	}
-// }
